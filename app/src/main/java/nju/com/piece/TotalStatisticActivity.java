bc6b7f574beaca4019fc8461025de78db37f9fe1
Jianwei <sjwjames@126.com>
2015-06-04 22:25:05 +0800

    layout

--- app/src/main/java/nju/com/piece/TotalStatisticActivity.java
+++ app/src/main/java/nju/com/piece/TotalStatisticActivity.java
@@ -1,17 +1,31 @@
 package nju.com.piece;
 
+import android.app.LocalActivityManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+
+import com.github.mikephil.charting.charts.BarChart;
 
 
 public class TotalStatisticActivity extends ActionBarActivity {
-
+    private TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_total_statistic);
+        tabHost=(TabHost)findViewById(R.id.tabhost);
+        LocalActivityManager localActivityManager=new LocalActivityManager(TotalStatisticActivity.this,true);
+        localActivityManager.dispatchCreate(savedInstanceState);
+        tabHost.setup(localActivityManager);
+        tabHost.addTab(tabHost.newTabSpec("daily_chart").setIndicator("每日").setContent(R.id.bar_charts));
+        tabHost.addTab(tabHost.newTabSpec("weekly_chart").setIndicator("每周").setContent(R.id.bar_charts));
+        tabHost.addTab(tabHost.newTabSpec("monthly_chart").setIndicator("每月").setContent(R.id.bar_charts));
+        BarChart dailyBar=(BarChart)findViewById(R.id.bar_charts);
+        BarChart weeklyBar=(BarChart)findViewById(R.id.bar_charts);
+        BarChart monthlyBar=(BarChart)findViewById(R.id.bar_charts);
     }
 
 
