b55fbd43ebba81d24db27711c1746b99945c4063
shen <2602889694@qq.com>
2015-06-06 21:58:39 +0800

    加了actionbar

---
+++ app/src/main/java/nju/com/piece/activity/PlusActionProvider.java
@@ -0,0 +1,44 @@
+package nju.com.piece.activity;
+
+import android.content.Context;
+import android.view.ActionProvider;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.SubMenu;
+import android.view.View;
+
+import nju.com.piece.R;
+
+public class PlusActionProvider extends ActionProvider {
+
+	private Context context;
+
+	public PlusActionProvider(Context context) {
+		super(context);
+		this.context = context;
+	}
+
+	@Override
+	public View onCreateActionView() {
+		return null;
+	}
+
+	@Override
+	public void onPrepareSubMenu(SubMenu subMenu) {
+		subMenu.clear();
+		subMenu.add(context.getString(R.string.plus_add_task))
+				.setIcon(R.drawable.action_add_tesk_icon)
+				.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+					@Override
+					public boolean onMenuItemClick(MenuItem item) {
+						return true;
+					}
+				});
+	}
+
+	@Override
+	public boolean hasSubMenu() {
+		return true;
+	}
+
+}
