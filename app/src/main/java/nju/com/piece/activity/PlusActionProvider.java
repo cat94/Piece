37d391249af26f82f6a3c422610818489ade01b2
shen <2602889694@qq.com>
2015-06-07 11:35:41 +0800

    美化了设置页面

--- app/src/main/java/nju/com/piece/activity/PlusActionProvider.java
+++ app/src/main/java/nju/com/piece/activity/PlusActionProvider.java
@@ -31,6 +31,7 @@ public class PlusActionProvider extends ActionProvider {
 				.setOnMenuItemClickListener(new OnMenuItemClickListener() {
 					@Override
 					public boolean onMenuItemClick(MenuItem item) {
+						
 						return true;
 					}
 				});
