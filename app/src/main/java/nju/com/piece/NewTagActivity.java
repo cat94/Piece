43a3f024a7bdd7d968991dc8dc7c09923c27664d
cat94 <CatMeetPet@gmail.com>
2015-06-08 21:04:31 +0800

    complete tag images

--- app/src/main/java/nju/com/piece/NewTagActivity.java
+++ app/src/main/java/nju/com/piece/NewTagActivity.java
@@ -34,7 +34,42 @@ public class NewTagActivity extends FragmentActivity implements OnDateSetListene
     private TextView plan_text;
 
     private GridView icon_grid;
-    private ArrayList<IconItem> icon_array = new ArrayList<IconItem>();
+    private static ArrayList<IconItem> icon_array = new ArrayList<IconItem>(){{
+        //        add icons
+        add(new IconItem(R.drawable.icon1_small));
+        add(new IconItem(R.drawable.icon2_small));
+        add(new IconItem(R.drawable.icon3_small));
+        add(new IconItem(R.drawable.icon4_small));
+        add(new IconItem(R.drawable.icon5_small));
+        add(new IconItem(R.drawable.icon6_small));
+        add(new IconItem(R.drawable.icon7_small));
+        add(new IconItem(R.drawable.icon8_small));
+        add(new IconItem(R.drawable.icon9_small));
+        add(new IconItem(R.drawable.icon10_small));
+        add(new IconItem(R.drawable.icon11_small));
+        add(new IconItem(R.drawable.icon12_small));
+        add(new IconItem(R.drawable.icon13_small));
+        add(new IconItem(R.drawable.icon14_small));
+        add(new IconItem(R.drawable.icon15_small));
+        add(new IconItem(R.drawable.icon16_small));
+        add(new IconItem(R.drawable.icon17_small));
+        add(new IconItem(R.drawable.icon2_small));
+        add(new IconItem(R.drawable.icon3_small));
+        add(new IconItem(R.drawable.icon4_small));
+        add(new IconItem(R.drawable.icon5_small));
+        add(new IconItem(R.drawable.icon6_small));
+        add(new IconItem(R.drawable.icon7_small));
+        add(new IconItem(R.drawable.icon8_small));
+        add(new IconItem(R.drawable.icon9_small));
+        add(new IconItem(R.drawable.icon10_small));
+        add(new IconItem(R.drawable.icon11_small));
+        add(new IconItem(R.drawable.icon12_small));
+        add(new IconItem(R.drawable.icon13_small));
+        add(new IconItem(R.drawable.icon14_small));
+        add(new IconItem(R.drawable.icon15_small));
+        add(new IconItem(R.drawable.icon16_small));
+        add(new IconItem(R.drawable.icon17_small));
+    }};
     private IconImageAdaptor icon_adaptor;
 
 
@@ -81,11 +116,7 @@ public class NewTagActivity extends FragmentActivity implements OnDateSetListene
         }
 
 
-//        add icons
-        icon_array.add(new IconItem(R.drawable.icon));
-        icon_array.add(new IconItem(R.drawable.icon));
-        icon_array.add(new IconItem(R.drawable.icon));
-        icon_array.add(new IconItem(R.drawable.icon));
+
 
         icon_adaptor = new IconImageAdaptor(this,R.layout.icon,icon_array);
 
